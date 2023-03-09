2732761278d1de31cc49343f04311ca00f7d739a
Bablushejul <shejulbablu@gmail.com>
2023-03-09 20:23:05 +0530

    hotel

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
-
+import Card from '../UI/Card';
+import MealItem from './MealItem';
 import classes from './AvailableMeals.module.css'
 
 const DUMY_MEALS=[{
@@ -10,11 +11,13 @@ const DUMY_MEALS=[{
 }];
 const AvailableMeals=()=>{
 
-    const mealList=DUMY_MEALS.map(meal=><li key={meal.id}>{meal.name}</li>)
+    const mealList=DUMY_MEALS.map((meal)=><MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
     return<section className={classes.meals}>
+        <Card>
         <ul>
             {mealList}
         </ul>
+        </Card>
     </section>
 
 };
