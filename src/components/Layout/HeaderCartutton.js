87c4a78fae3df41df346eafab4f4c2f896805b61
Bablushejul <shejulbablu@gmail.com>
2023-03-10 17:40:11 +0530

    Cartitems

--- src/components/Layout/HeaderCartutton.js
+++ src/components/Layout/HeaderCartutton.js
@@ -1,13 +1,19 @@
+import React,{useContext} from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../Store/cart-context";
 import classes from "./HeaderCartutton.module.css";
 const HeaderCartutton = (props) => {
+ const cartCtx= useContext(CartContext);
+ const numberOfCartItems=cartCtx.items.reduce((currNumber,item)=>{
+  return currNumber+item.amount;
+ },0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
