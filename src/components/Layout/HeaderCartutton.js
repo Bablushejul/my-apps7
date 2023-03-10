16afa69bf3a7e3655f68d1a5b99a7df96ad9984a
Bablushejul <shejulbablu@gmail.com>
2023-03-10 15:07:15 +0530

    Hotel

--- src/components/Layout/HeaderCartutton.js
+++ src/components/Layout/HeaderCartutton.js
@@ -1,18 +1,15 @@
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartutton.module.css';
-const HeaderCartutton=props=>{
-    return(
-       
-    <button className={classes.button}>
-        <span className={classes.icon}>
-<CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+import classes from "./HeaderCartutton.module.css";
+const HeaderCartutton = (props) => {
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
     </button>
-    
-    )
-
-}
+  );
+};
 
 export default HeaderCartutton;
